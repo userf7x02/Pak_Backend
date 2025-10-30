de33ac7b4ec33c3bb3ccb11349a953b327c83119
Hamza Malik <webforge3333@gmail.com>
2025-10-30 17:04:00 +0500

    add manual CORS header for guaranted fix

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const mongoose = require('mongoose');
 
 require("dotenv").config();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// Phir CORS package (optional)
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   credentials: true
 }));
-
 // ✅ MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
